a699bb94b732a12ca5b94c9c259574bec9409331
Frenson7hotang <frensonpekanbaru2019@gmail.com>
2023-04-12 20:50:16 +0700

    laravel intro

---
+++ example-app/resources/views/halaman.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome</title>
+</head>
+<body>
+<h1>Selamat Datang {{$nama1}} {{$nama2}} </h1>
+<p>Terima Kasih Telah bergabung bersama Garuda Cyber Institute</p>
+</body>
+</html>
